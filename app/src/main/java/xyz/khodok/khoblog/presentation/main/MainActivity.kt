e644cba977070d2ef03b433c61042f2876f0e6cd
Julien Rutscho <julien.rutscho@ceff.ch>
2021-06-23 12:55:47 +0200

    rename activity_main

--- app/src/main/java/xyz/khodok/khoblog/presentation/main/MainActivity.kt
+++ app/src/main/java/xyz/khodok/khoblog/presentation/main/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        setContentView(R.layout.activity_main)
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
 
