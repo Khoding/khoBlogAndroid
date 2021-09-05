7c07a71ed4c060f9b50504115c5f56020fc22dab
Khoding <julien.rutscho@ceff.ch>
2021-09-05 13:08:38 +0200

    just a smol reformat

--- app/src/main/java/xyz/khodok/khoblog/presentation/main/MainActivity.kt
+++ app/src/main/java/xyz/khodok/khoblog/presentation/main/MainActivity.kt
@@ -2,7 +2,6 @@ package xyz.khodok.khoblog.presentation.main
 
 import android.os.Bundle
 import android.view.Menu
-import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
